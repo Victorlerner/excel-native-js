548ae8ea7c527dc66d0e5456fb8e0d6ab596a4e7
Victorlerner <leonn366@gmail.com>
2020-06-03 14:59:54 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,20 +3,33 @@ const CODES = {
   Z: 90,
 }
 
-function toCell(val) {
-  return `<div class="cell" contenteditable="true">${val}</div>`
+function toCell(_, col) {
+
+  return `
+    <div class="cell" data-col="${col}" contenteditable></div>
+  `
 }
 
-function toColumn(col) {
-  return `<div class="column">${col}</div>`
+function toColumn(col,index) {
+  return `
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row" ></div>' : ''
   return `
-        <div class="row">
-                <div class="row-info">${index ? index : ''}</div>
-                <div class="row-data"> ${content}</div>
-        </div>`
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
 }
 
 function toChar(_, index) {
@@ -24,16 +37,18 @@ function toChar(_, index) {
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
-  console.log(cols)
+
   rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    const cels = new Array(colsCount).fill('').map(toCell).join('')
-    rows.push(createRow( i+1, cels))
+    const cells = new Array(colsCount).fill('').map(toCell).join('')
+
+    rows.push(createRow(i + 1, cells))
   }
+
   return rows.join('')
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -23,7 +23,6 @@ class Dom {
   }
 
   on(eventType, callback) {
-
     this.$el.addEventListener(eventType, callback)
   }
 
@@ -42,6 +41,25 @@ class Dom {
       this.$el.appendChild(node)
     }
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => this.$el.style[key] = styles[key])
   }
 }
 
